d3f74889aa103115500b32838a25d32905728c2a
Sarthakgoell <33325394+Sarthakgoell@users.noreply.github.com>
2019-05-15 20:24:59 +0800

    Update camera_first.py

--- camera_first.py
+++ camera_first.py
@@ -4,12 +4,27 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.start_preview()
+#transparency of camera screen (from 0 --> transparent to 255 --> opaque)
+camera.start_preview(#alpha=200)
+#to take video
 camera.start_recording('/home/pi/Desktop/TestingBelt2.h264')
+
+#the time camera will run for.
 
 sleep(30)
 
+#to take picture.
+#camera.capture('/home/pi/Desktop/image.jpg')
+
+#camera.rotation
+#camera.rotation=180 #value can be 90,180,270,0
+
+
 camera.stop_recording()
 camera.stop_preview()
+  
+#when playing video use: 
+  #omxplayer /home/pi/Desktop/TestingBelt2.h264(from the top)
+  
 
 
